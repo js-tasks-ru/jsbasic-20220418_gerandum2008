ddb7dc5d6c3182deb51eb97dcffc87250d775ed9
gerandum2008 <gerandum2008@ro.ru>
2022-04-24 21:45:46 +0300

    фикс 3 задачи

--- 1-module/3-task/index.js
+++ 1-module/3-task/index.js
@@ -1,6 +1,6 @@
 function ucFirst(str) {
 	if (!str || 0 === str.length) { return str } else {
 		let big = str[0].toUpperCase()
-		return(big + str.slice(1, str.length))
+		return(big + str.slice(1))
 	}
 }
