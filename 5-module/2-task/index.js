6892d37d7f1000e873af12c73c00ee04c2376ec5
Gerandum2008 <gerandum2008@ro.ru>
2022-05-17 14:29:15 +0300

    модуль 5

--- 5-module/2-task/index.js
+++ 5-module/2-task/index.js
@@ -1,3 +1,11 @@
 function toggleText() {
-  // ваш код...
+	let btn = document.querySelector('.toggle-text-button');
+	let text = document.querySelector('#text');
+	btn.addEventListener('click', function () {
+		if (text.hidden) {
+			text.hidden = false;
+			return;
+		}
+		text.hidden = true;
+	})
 }
--- 5-module/3-task/index.js
+++ 5-module/3-task/index.js
@@ -1,3 +1,26 @@
 function initCarousel() {
-  // ваш код...
+	let right = document.querySelector(".carousel__arrow_right");
+	let left = document.querySelector(".carousel__arrow_left");
+	let slide = document.querySelector(".carousel__inner");
+	let offsetWidthSlide = slide.offsetWidth;
+	left.style.display = 'none';
+	slide.style.transform = 'translateX(0px)';
+	let count = 0;
+
+	let adress = function () {
+		left.style.display = '';
+		if (count === 2) { right.style.display = 'none' }
+		count++
+		slide.style.transform = `translateX(${-offsetWidthSlide * count}px)`
+	};
+
+	let leftAdress = function () {
+		right.style.display = ''
+		if (count === 1) { left.style.display = 'none' }
+		count--;
+		slide.style.transform = `translateX(${-offsetWidthSlide * count}px)`;
+	};
+
+	right.addEventListener("click", adress);
+	left.addEventListener("click", leftAdress);
 }
